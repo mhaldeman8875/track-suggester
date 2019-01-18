31250d00ca40e9948e6577e566454c2ff3ff4893
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-01-18 14:27:02 -0800

    write first rendition of JS using branches to display different results based on answer values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,81 @@
 $(document).ready(function() {
+
     $("#formOne").submit(function(event) {
-      var name = $("input#name").val();
+        var name = $("input#name").val();
 
-      $(".name").text(name);
-      $("#landing").hide();
-      $("#survey").show();
+        $(".name").text(name);
+        $("#landing").hide();
+        $("#survey").show();
 
-      event.preventDefault();
+        event.preventDefault();
+    });
+
+    $("#formTwo").submit(function(event) {
+        var experience = parseInt($("select#experience").val());
+        var language = parseInt($("select#language").val());
+        var stack = parseInt($("select#stack").val());
+        var industry = parseInt($("select#industry").val());
+        var company = parseInt($("select#company").val());
+        var size = parseInt($("select#size").val());
+
+     
+        if (experience === 1 && 
+            stack   === 3 || 
+            company === 3 || 
+            language === 2) {
+            
+            $("#cSharpResult").show();
+            $("#javaResult").hide();
+            $("#phpResult").hide();
+            $("#rubyResult").hide();
+            $("#cssResult").hide();
+            $("#survey").hide();
+        
+        } else if (experience === 3 && 
+            stack === 2 && 
+            industry === 2 || 
+            language === 5) {
+                
+            $("#cSharpResult").hide();
+            $("#javaResult").hide();
+            $("#phpResult").show();
+            $("#rubyResult").hide();
+            $("#cssResult").hide();
+            $("#survey").hide();
+
+        } else if (experience === 2 &&
+            (company === 1 || 4) && 
+            stack === 1 &&
+            size === 1) { 
+
+            $("#cSharpResult").hide();
+            $("#javaResult").hide();
+            $("#phpResult").hide();
+            $("#rubyResult").show();
+            $("#cssResult").hide();
+            $("#survey").hide();
+
+        } else if (language === 4 || 
+            ((experience === 4 || 1)  && 
+            (company === 1 || 4) && 
+            stack === 1)) { 
+                   
+            $("#cSharpResult").hide();
+            $("#javaResult").hide();
+            $("#phpResult").hide();
+            $("#rubyResult").hide();
+            $("#cssResult").show();
+            $("#survey").hide();
+
+        } else { 
+
+            $("#cSharpResult").hide();
+            $("#javaResult").show();
+            $("#phpResult").hide();
+            $("#rubyResult").hide();
+            $("#cssResult").hide();
+            $("#survey").hide();
+        
+        }
     });
 });
